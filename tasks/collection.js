5b2fd0599aab302e3488d7befb6a78dbf9b476ce
Hannes Diercks <hi@xiphe.net>
2015-01-13 00:10:09 +0100

    fix(loadTasks): start import at project dir not cwd

--- tasks/collection.js
+++ tasks/collection.js
@@ -1,6 +1,8 @@
+/* global __dirname */
 var grunt = require('grunt');
 var findup = require('findup-sync');
 var path = require('path');
+var basepath = path.resolve(__dirname, '..');
 var dependencies = require('../package.json').dependencies;
 var dependency;
 var tasksDir;
@@ -9,7 +11,7 @@ grunt.verbose.subhead('Registering subtasks of "grunt-angular-toolbox"...');
 
 for (dependency in dependencies) {
   if (dependency.indexOf('grunt-') === 0) {
-    tasksDir = findup(path.join('node_modules', dependency, 'tasks'));
+    tasksDir = findup(path.join('node_modules', dependency, 'tasks'), {cwd: basepath});
     if (tasksDir) {
       grunt.loadTasks(tasksDir);
     }
