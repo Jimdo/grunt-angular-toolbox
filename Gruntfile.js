05f093cdada5b7412dc51701bb0ecf454dbb14af
Hannes Diercks <hi@xiphe.net>
2015-05-31 16:05:25 +0200

    chore: add jshint and jscs checkers

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,16 +21,34 @@ module.exports = function(grunt) {
       test: {
         command: 'cd sample_project && ../node_modules/.bin/grunt test'
       }
+    },
+    jshint: {
+      src: [
+        'lib/**/*.js',
+        'tasks/**/*.js',
+        '*.js'
+      ],
+      options: {
+        jshintrc: true
+      }
+    },
+    jscs: {
+      src: '<%= jshint.src %>'
     }
   });
 
   /* Load grunt tasks from NPM packages */
   grunt.loadNpmTasks('grunt-npm');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-jscs');
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-bump');
 
-
-  grunt.registerTask('test', ['shell:test']);
+  grunt.registerTask('test', [
+    'jshint',
+    'jscs',
+    'shell:test'
+  ]);
 
   grunt.registerTask(
     'release',
