0e65e0a655c7212135b7cbf327251cc3e5d2aabc
jadismail91 <jad.ismail@jimdo.com>
2022-03-23 14:03:05 +0100

    Migrate from TravisCI to GitHub actions CRE-1204

--- sample_project/test/e2e/SpecHelper.js
+++ sample_project/test/e2e/SpecHelper.js
@@ -2,6 +2,7 @@ var port = process.env.E2E_SANDBOX_PORT || 8765;
 var sandboxUrl = 'http://localhost:' + port + '/';
 
 beforeEach(function() {
+  browser.waitForAngularEnabled(false);
   browser.get(sandboxUrl);
 });
 
